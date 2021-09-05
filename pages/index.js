8d39577746a4b7c8bf0c8b7e643f047d04737075
FabienStervinou <stervinoufabien@gmail.com>
2021-09-05 11:30:22 +0200

    Change photographer name file

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
+// TODO: remove
+console.log('File init')
+
 // DOM Elmement
 let modalContact = document.getElementById('modalContact')
 let contactBtn = document.getElementById('contactBtn')
@@ -10,14 +13,18 @@ submitBtn.addEventListener('click', submitFormContact)
 
 function openModalContact() {
   modalContact.style.display = 'block'
+  console.log('Form click')
 }
 
 function closeModalContact() {
   modalContact.style.display = 'none'
+  console.log('Form close')
 }
 
 function submitFormContact(e) {
   e.preventDefault()
+
+  console.log('Form click')
 
   // Input 
   let firstnameInput = document.getElementById('firstname')
@@ -26,42 +33,42 @@ function submitFormContact(e) {
   let messageInput = document.getElementById('message')
 
   // Regex
-  let emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  let onlyLetterRegex = /[^a-zA-Z]/;
+  let emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+  let onlyLetterRegex = /[^a-zA-Z]/
 
   let isValidate = true
 
   if (firstnameInput.value.length < 2 || onlyLetterRegex.test(firstnameInput.value)) {
-    firstnameInput.parentNode.dataset.errorVisible = true;
-    isValidate = false;
+    firstnameInput.parentNode.dataset.errorVisible = true
+    isValidate = false
   } else {
-    firstnameInput.parentNode.dataset.errorVisible = false;
+    firstnameInput.parentNode.dataset.errorVisible = false
   }
 
   if (lastnameInput.value.length < 2 || onlyLetterRegex.test(lastnameInput.value)) {
-    lastnameInput.parentNode.dataset.errorVisible = true;
-    isValidate = false;
+    lastnameInput.parentNode.dataset.errorVisible = true
+    isValidate = false
   } else {
-    lastnameInput.parentNode.dataset.errorVisible = false;
+    lastnameInput.parentNode.dataset.errorVisible = false
   }
 
   if (emailRegex.test(emailInput.value) == false) {
-    emailInput.parentNode.dataset.errorVisible = true;
-    isValidate = false;
+    emailInput.parentNode.dataset.errorVisible = true
+    isValidate = false
   } else {
-    emailInput.parentNode.dataset.errorVisible = false;
+    emailInput.parentNode.dataset.errorVisible = false
   }
 
   if (messageInput.value.length <= 0 || messageInput.value.length > 500) {
-    messageInput.parentNode.dataset.errorVisible = true;
-    isValidate = false;
+    messageInput.parentNode.dataset.errorVisible = true
+    isValidate = false
   } else {
-    messageInput.parentNode.dataset.errorVisible = false;
+    messageInput.parentNode.dataset.errorVisible = false
   }
 
   if (isValidate == true) {
-    document.forms["formContact"].submit();
+    document.forms["formContact"].submit()
   }
 
-  return isValidate;
+  return isValidate
 }
